5cb05786a38c01e53a0fd3b7b6a3ec42b1cd96cf
Samjuel1 <samuel.garciadios@edu.uah.es>
2025-04-18 00:51:04 +0200

    Cambios en mapa

--- resources/maps/[A]Flower_pipo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="[A]Flower_pipo" tilewidth="32" tileheight="32" tilecount="96" columns="8">
- <image source="../tilesets/[A]Flower_pipo.png" width="256" height="384"/>
-</tileset>
