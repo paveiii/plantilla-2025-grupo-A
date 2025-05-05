89f8126f5e1a13458a4e9ce195ea00106d4fff1f
PabloAlvaroPS <pabloalvaro.pena@edu.uah.es>
2025-05-05 04:26:19 +0200

    2º avance Mapa de Cyberpunk

--- resources/tilesets/Tilesets Cyberpunk/Archivos.tsx/Suburbio Casa Mediana.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="Suburbio Casa Mediana" tilewidth="32" tileheight="32" tilecount="54" columns="6">
- <image source="../Suburbio Casa Mediana.png" width="192" height="288"/>
-</tileset>
