f3ac6ea0254540a0897170bc4621ad1db6b75d5e
paveiii <pavel.tabata@edu.uah.es>
2025-05-22 18:51:26 +0200

    Cambios menores a load_game_view.py

--- rpg/views/load_game_view.py
+++ rpg/views/load_game_view.py
@@ -7,13 +7,29 @@ from rpg.views.menu_view import MenuView
 class LoadGameView(arcade.View):
     def __init__(self):
         super().__init__()
-        self.started = False
         arcade.set_background_color(arcade.color.ALMOND)
+
+        # --- Required for all code that uses UI element, a UIManager to handle the UI.
+        self.manager = arcade.gui.UIManager()
+
+        # Create a vertical BoxGroup to align buttons
+        self.v_box = arcade.gui.UIBoxLayout()
+
+        for i in range(12):
+            newButton = SaveFileButton(text=str(i), width=500, height=45)
+            self.v_box.add(newButton)
+
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="center_x", anchor_y="center_y", align_y=-50, child=self.v_box
+            )
+        )
 
     def on_draw(self):
         arcade.start_render()
+        self.manager.draw()
         arcade.draw_text(
-            "load game",
+            "Load game",
             self.window.width / 2,
             self.window.height - 50,
             arcade.color.ALLOY_ORANGE,
@@ -28,8 +44,12 @@ class LoadGameView(arcade.View):
         pass
 
     def on_show_view(self):
+        self.manager.enable()
         arcade.set_background_color(arcade.color.ALMOND)
         arcade.set_viewport(0, self.window.width, 0, self.window.height)
+
+    def on_hide_view(self):
+        self.manager.disable()
 
     def on_key_press(self, symbol: int, modifiers: int):
         close_inputs = [
@@ -40,3 +60,7 @@ class LoadGameView(arcade.View):
     def on_update(self, delta_time: float):
         self.window.views["menu"] = MenuView()
 
+class SaveFileButton(arcade.gui.UIFlatButton):
+
+    def on_click(self, event: arcade.gui.UIOnClickEvent):
+        print(self.text)
