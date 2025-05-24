744e0f8e633804484b191e8d64a26825ef013ee7
PabloAlvaroPS <pabloalvaro.pena@edu.uah.es>
2025-05-24 21:16:53 +0200

    Mapa del interior del Time Company (Con animacion de Portal)

---
+++ resources/tilesets/Tilesets Cyberpunk/Archivos.tsx/Portal del tiempo (4).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Portal del tiempo (4)" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../Portal del tiempo (4).png" width="96" height="96"/>
+</tileset>
