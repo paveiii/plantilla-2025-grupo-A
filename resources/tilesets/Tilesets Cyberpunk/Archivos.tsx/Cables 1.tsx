ffa74e20d8010c6f36bbcfc0ec01bf2fe10ad0f8
PabloAlvaroPS <pabloalvaro.pena@edu.uah.es>
2025-05-25 02:05:47 +0200

    Mapa del interior de la Power Plant (Lugar de la batalla final)

---
+++ resources/tilesets/Tilesets Cyberpunk/Archivos.tsx/Cables 1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Cables 1" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../Cables 1.png" width="32" height="32"/>
+</tileset>
